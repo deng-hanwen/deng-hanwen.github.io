ef752b7570ff12bc69f7bfdcd454e48af9771c46
deng-hanwen <denghanwen0513@gmail.com>
2026-05-19 17:43:06 +0000

    Deploying to gh-pages from @ deng-hanwen/deng-hanwen.github.io@723795c24dbf5e003b23772bf9a892abce769195 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,9 +9,9 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+  },{id: "nav-research",
+          title: "Research",
+          description: "Computational psychiatry, neuroimaging, and EMA research projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
